94e14b7279d1e6bddf236eab526e838e35016610
Lucas Nogueira <lucasgo1099@gmail.com>
2021-10-25 08:06:55 -0300

    add time page in navigator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:br/models/time.dart';
 import 'package:br/pages/home_controller.dart';
+import 'package:br/pages/time_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -25,16 +26,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lista de Clubes'),
+        title: Text('Brasileirão Clubes'),
       ),
       body: ListView.separated(
         itemCount: controller.tabela.length,
-        itemBuilder: (BuildContext context, int i) {
+        itemBuilder: (BuildContext context, int time) {
           final List<Time> tabela = controller.tabela;
           return ListTile(
-            leading: Image.network(tabela[i].brasao),
-            title: Text(tabela[i].nome),
-            trailing: Text(tabela[i].pontos.toString()),
+            leading: Image.network(tabela[time].brasao),
+            title: Text(tabela[time].nome),
+            trailing: Text(tabela[time].pontos.toString()),
+            //quando o usuário pressecionar
+            //navegação para a página time PAGE
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (_) => TimePage(
+                          key: Key(tabela[time].nome), time: tabela[time])));
+            },
           );
         },
         separatorBuilder: (_, __) => Divider(),
--- lib/pages/time_page.dart
+++ lib/pages/time_page.dart
@@ -12,12 +12,46 @@ class TimePage extends StatefulWidget {
 class _TimePageState extends State<TimePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        //acessando a cor do time
-        backgroundColor: widget.time.cor,
-        title: Text(widget.time.nome),
-      ),
-    );
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            //acessando a cor do time
+            backgroundColor: widget.time.cor,
+            title: Text(widget.time.nome),
+            //colocando as taps, onde vamos add os titulos
+            //veremos as estatisticas
+            bottom: const TabBar(tabs: [
+              Tab(
+                icon: Icon(Icons.stacked_line_chart),
+                text: 'Estatisticas',
+              ),
+              Tab(
+                icon: Icon(Icons.emoji_events),
+                text: 'Títulos',
+              ),
+            ], indicatorColor: Colors.white),
+          ),
+          body: TabBarView(children: [
+            //Paginas que vamos carregar
+            Column(
+              //alinhando coluna ao centro
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //add a margem pra não ficar grudado
+                Padding(
+                  padding: EdgeInsets.all(24),
+                  child: Image.network(
+                      widget.time.brasao.replaceAll('40x40', "100x100")),
+                ),
+                Text(
+                  'Pontos: ${widget.time.pontos}',
+                  style: TextStyle(fontSize: 22),
+                )
+              ],
+            ),
+            Container()
+          ]),
+        ));
   }
 }
